de2c49ff30bc96983b249e6b9106bc94913579ca
esraa_eissa123 <esraa.eissa123@gmail.com>
2021-11-25 22:37:32 +0200

    commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,27 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Spatie\MediaLibrary\Models\Media;
-use Spatie\MediaLibrary\HasMedia\HasMedia;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Product extends Model implements HasMedia
 {
     protected $fillable = [
         'name', 'detail', 'quantity','price'
     ];
-    use HasFactory, HasMediaTrait ;
+    use HasFactory, InteractsWithMedia;
     public function brands()
     {
         return $this->belongsToMany('App\Models\Brand');
     }
-    public function registerMediaCollections(): void
-    {
-        $this->addMediaCollection('avatar');
-    }
-
-    public function registerMediaConversions(Media $media = null): void
-    {
-        $this->addMediaConversion('thumb')->width(50)->height(50);
-    }
 }
-
