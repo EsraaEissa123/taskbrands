84409177444ed70b692ef6739e214a7582946c84
esraa_eissa123 <esraa.eissa123@gmail.com>
2022-06-19 04:39:01 +0200

    commit

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
-// use Spatie\MediaLibrary\HasMedia\HasMediaTrait;
+// use Spatie\MediaLibrary\HasMedia;
 
 
 
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -670,7 +670,7 @@
             <!--=================================
  footer -->
 
-            @include('layouts.footer')
+            {{-- @include('layouts.footer') --}}
         </div><!-- main content wrapper end-->
     </div>
     </div>
